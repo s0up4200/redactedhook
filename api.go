a649bfeb82b623dbaaeec02d49883fc603ed6124
soup <soup@r4tio.dev>
2023-11-15 12:00:07 +0100

    make logger configurable via config.toml

---
+++ api.go
@@ -0,0 +1,426 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/inhies/go-bytesize"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+	"golang.org/x/time/rate"
+)
+
+const (
+	APIEndpointBaseRedacted = "https://redacted.ch/ajax.php"
+	APIEndpointBaseOrpheus  = "https://orpheus.network/ajax.php"
+	Pathhook                = "/hook"
+)
+
+const ( // HTTP status codes for custom logic
+	StatusUploaderNotAllowed = http.StatusIMUsed + 1
+	StatusLabelNotAllowed    = http.StatusIMUsed + 2
+	StatusSizeNotAllowed     = http.StatusIMUsed + 3
+	StatusRatioNotAllowed    = http.StatusIMUsed
+)
+
+var redactedLimiter = rate.NewLimiter(rate.Every(10*time.Second), 10)
+var orpheusLimiter = rate.NewLimiter(rate.Every(10*time.Second), 5)
+
+type RequestData struct {
+	REDUserID   int               `json:"red_user_id,omitempty"`
+	OPSUserID   int               `json:"ops_user_id,omitempty"`
+	TorrentID   int               `json:"torrent_id,omitempty"`
+	REDKey      string            `json:"red_apikey,omitempty"`
+	OPSKey      string            `json:"ops_apikey,omitempty"`
+	MinRatio    float64           `json:"minratio,omitempty"`
+	MinSize     bytesize.ByteSize `json:"minsize,omitempty"`
+	MaxSize     bytesize.ByteSize `json:"maxsize,omitempty"`
+	Uploaders   string            `json:"uploaders,omitempty"`
+	RecordLabel string            `json:"record_labels,omitempty"`
+	Mode        string            `json:"mode,omitempty"`
+	Indexer     string            `json:"indexer"`
+	TorrentName string            `json:"torrentname,omitempty"`
+}
+
+type ResponseData struct {
+	Status   string `json:"status"`
+	Error    string `json:"error"`
+	Response struct {
+		Username string `json:"username"`
+		Stats    struct {
+			Ratio float64 `json:"ratio"`
+		} `json:"stats"`
+		Group struct {
+			Name      string `json:"name"`
+			MusicInfo struct {
+				Artists []struct {
+					ID   int    `json:"id"`
+					Name string `json:"name"`
+				} `json:"artists"`
+			} `json:"musicInfo"`
+		} `json:"group"`
+		Torrent *struct {
+			Username        string `json:"username"`
+			Size            int64  `json:"size"`
+			RecordLabel     string `json:"remasterRecordLabel"`
+			ReleaseName     string `json:"filePath"`
+			CatalogueNumber string `json:"remasterCatalogueNumber"`
+		} `json:"torrent"`
+	} `json:"response"`
+}
+
+// fetchAPI does a rate-limited API call and unmarshals the response.
+func fetchAPI(endpoint, apiKey string, limiter *rate.Limiter, indexer string, target interface{}) error {
+	if !limiter.Allow() {
+		log.Warn().Msgf("%s: Too many requests", indexer)
+		return fmt.Errorf("too many requests")
+	}
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", apiKey)
+
+	client := &http.Client{Timeout: time.Second * 10}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(respBody, target); err != nil {
+		return err
+	}
+
+	responseData := target.(*ResponseData)
+	if responseData.Status != "success" {
+		log.Warn().Msgf("API error from %s: %s", indexer, responseData.Error)
+		return fmt.Errorf("API error from %s: %s", indexer, responseData.Error)
+	}
+
+	return nil
+}
+
+func fetchTorrentData(torrentID int, apiKey, apiBase, indexer string) (*ResponseData, error) {
+	limiter := getLimiter(indexer)
+	if limiter == nil {
+		return nil, fmt.Errorf("could not get rate limiter for indexer: %s", indexer)
+	}
+
+	endpoint := fmt.Sprintf("%s?action=torrent&id=%d", apiBase, torrentID)
+	responseData := &ResponseData{}
+	if err := fetchAPI(endpoint, apiKey, limiter, indexer, responseData); err != nil {
+		return nil, err
+	}
+
+	// Log the release information
+	if responseData.Response.Torrent != nil {
+		releaseName := responseData.Response.Torrent.ReleaseName
+		uploader := responseData.Response.Torrent.Username
+		log.Debug().Msgf("Checking release: %s - (Uploader: %s) (TorrentID: %d)", releaseName, uploader, torrentID)
+	}
+
+	return responseData, nil
+}
+
+func fetchUserData(userID int, apiKey, indexer, apiBase string) (*ResponseData, error) {
+	limiter := getLimiter(indexer)
+	endpoint := fmt.Sprintf("%s?action=user&id=%d", apiBase, userID)
+	responseData := &ResponseData{}
+	if err := fetchAPI(endpoint, apiKey, limiter, indexer, responseData); err != nil {
+		return nil, err
+	}
+	return responseData, nil
+}
+
+func getLimiter(indexer string) *rate.Limiter {
+	switch indexer {
+	case "redacted":
+		return redactedLimiter
+	case "ops":
+		return orpheusLimiter
+	default:
+		log.Error().Msgf("Invalid indexer: %s", indexer)
+		return nil
+	}
+}
+
+func hookData(w http.ResponseWriter, r *http.Request) {
+
+	var torrentData *ResponseData
+	var userData *ResponseData
+	var requestData RequestData
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method is supported", http.StatusBadRequest)
+		return
+	}
+
+	// Read JSON payload from the request body
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = json.Unmarshal(body, &requestData)
+	if err != nil {
+		log.Error().Msgf("Failed to unmarshal JSON payload: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Unmarshal the configuration
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Unable to decode into struct")
+	}
+
+	if requestData.Indexer != "ops" && requestData.Indexer != "redacted" {
+		if requestData.Indexer == "" {
+			log.Error().Msg("No indexer provided")
+			http.Error(w, "no indexer provided", http.StatusBadRequest)
+		} else {
+			log.Error().Msgf("Invalid indexer: %s", requestData.Indexer)
+			http.Error(w, fmt.Sprintf("Invalid indexer: %s", requestData.Indexer), http.StatusBadRequest)
+		}
+		return
+	}
+
+	// Check each field in requestData and fallback to config if empty
+	if requestData.REDUserID == 0 {
+		requestData.REDUserID = config.UserID.REDUserID
+	}
+	if requestData.OPSUserID == 0 {
+		requestData.OPSUserID = config.UserID.OPSUserID
+	}
+	if requestData.REDKey == "" {
+		requestData.REDKey = config.APIKeys.REDKey
+	}
+	if requestData.OPSKey == "" {
+		requestData.OPSKey = config.APIKeys.OPSKey
+	}
+	if requestData.MinRatio == 0 {
+		requestData.MinRatio = config.Ratio.MinRatio
+	}
+	if requestData.MinSize == 0 {
+		requestData.MinSize = bytesize.ByteSize(config.ParsedSizes.MinSize)
+	}
+	if requestData.MaxSize == 0 {
+		requestData.MaxSize = bytesize.ByteSize(config.ParsedSizes.MaxSize)
+	}
+	if requestData.Uploaders == "" {
+		requestData.Uploaders = config.Uploaders.Uploaders
+	}
+	if requestData.Mode == "" {
+		requestData.Mode = config.Uploaders.Mode
+	}
+
+	// Log request received
+	logMsg := fmt.Sprintf("Received data request from %s", r.RemoteAddr)
+	log.Info().Msg(logMsg)
+
+	// Determine the appropriate API base based on the requested hook path
+	var apiBase string
+	switch requestData.Indexer {
+	case "redacted":
+		apiBase = APIEndpointBaseRedacted
+	case "ops":
+		apiBase = APIEndpointBaseOrpheus
+	default:
+		http.Error(w, "Invalid path", http.StatusNotFound)
+		return
+	}
+
+	reqHeader := make(http.Header)
+	var apiKey string
+	if requestData.Indexer == "redacted" {
+		apiKey = requestData.REDKey
+	} else if requestData.Indexer == "ops" {
+		apiKey = requestData.OPSKey
+	}
+	reqHeader.Set("Authorization", apiKey)
+
+	var cfg Config
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Unable to decode into struct")
+	}
+
+	// hook uploader
+	if requestData.TorrentID != 0 && requestData.Uploaders != "" {
+		if torrentData == nil {
+			var apiKey string
+			if requestData.Indexer == "redacted" {
+				apiKey = requestData.REDKey
+			} else if requestData.Indexer == "ops" {
+				apiKey = requestData.OPSKey
+			}
+			torrentData, err = fetchTorrentData(requestData.TorrentID, apiKey, apiBase, requestData.Indexer)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		username := torrentData.Response.Torrent.Username
+		usernames := strings.Split(requestData.Uploaders, ",")
+
+		for i, username := range usernames { // Trim whitespace from each username
+			usernames[i] = strings.TrimSpace(username)
+		}
+		usernamesStr := strings.Join(usernames, ", ") // Join the usernames with a comma and a single space
+		log.Trace().Msgf("Requested uploaders [%s]: %s", requestData.Mode, usernamesStr)
+
+		isListed := false
+		for _, uname := range usernames {
+			if uname == username {
+				isListed = true
+				break
+			}
+		}
+
+		if (requestData.Mode == "blacklist" && isListed) || (requestData.Mode == "whitelist" && !isListed) {
+			w.WriteHeader(StatusUploaderNotAllowed)
+			log.Debug().Msgf("Uploader (%s) is not allowed", username)
+			return
+		}
+	}
+
+	// hook record label
+	if requestData.TorrentID != 0 && requestData.RecordLabel != "" {
+		if torrentData == nil {
+			var apiKey string
+			if requestData.Indexer == "redacted" {
+				apiKey = requestData.REDKey
+			} else if requestData.Indexer == "ops" {
+				apiKey = requestData.OPSKey
+			}
+			torrentData, err = fetchTorrentData(requestData.TorrentID, apiKey, apiBase, requestData.Indexer)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		recordLabel := torrentData.Response.Torrent.RecordLabel
+		name := torrentData.Response.Group.Name
+		//releaseName := torrentData.Response.Torrent.ReleaseName
+		requestedRecordLabels := strings.Split(requestData.RecordLabel, ",")
+
+		if recordLabel == "" {
+			log.Debug().Msgf("No record label found for release: %s", name)
+			w.WriteHeader(StatusLabelNotAllowed)
+			return
+		}
+
+		recordlabelsStr := strings.Trim(fmt.Sprint(requestedRecordLabels), "[]")
+		log.Trace().Msgf("Requested record labels: %v", recordlabelsStr)
+
+		isRecordLabelPresent := false
+		for _, rLabel := range requestedRecordLabels {
+			if rLabel == recordLabel {
+				isRecordLabelPresent = true
+				break
+			}
+		}
+
+		if !isRecordLabelPresent {
+			w.WriteHeader(StatusLabelNotAllowed)
+			log.Debug().Msgf("The record label '%s' is not included in the requested record labels: %v", recordLabel, requestedRecordLabels)
+			return
+		}
+	}
+
+	// hook size
+	if requestData.TorrentID != 0 && (requestData.MinSize != 0 || requestData.MaxSize != 0) {
+		if torrentData == nil {
+			var apiKey string
+			if requestData.Indexer == "redacted" {
+				apiKey = requestData.REDKey
+			} else if requestData.Indexer == "ops" {
+				apiKey = requestData.OPSKey
+			}
+			torrentData, err = fetchTorrentData(requestData.TorrentID, apiKey, apiBase, requestData.Indexer)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		torrentSize := bytesize.ByteSize(torrentData.Response.Torrent.Size)
+
+		minSize := bytesize.ByteSize(requestData.MinSize)
+		maxSize := bytesize.ByteSize(requestData.MaxSize)
+
+		log.Trace().Msgf("Torrent size: %s, Requested size range: %s - %s", torrentSize, requestData.MinSize, requestData.MaxSize)
+
+		if (requestData.MinSize != 0 && torrentSize < minSize) ||
+			(requestData.MaxSize != 0 && torrentSize > maxSize) {
+			w.WriteHeader(StatusSizeNotAllowed)
+			log.Debug().Msgf("Torrent size %s is outside the requested size range: %s to %s", torrentSize, minSize, maxSize)
+			return
+		}
+	}
+
+	// hook ratio
+	if requestData.MinRatio != 0 {
+		var userID int
+		var apiKey string
+		if requestData.Indexer == "redacted" {
+			if requestData.REDUserID == 0 {
+				log.Error().Msg("red_user_id is missing but required when minratio is set for 'redacted'")
+				http.Error(w, "red_user_id is required for 'redacted' when minratio is set", http.StatusBadRequest)
+				return
+			}
+			userID = requestData.REDUserID
+			apiKey = requestData.REDKey
+			//log.Trace().Msgf("MinRatio check for Redacted with user ID: %d", userID)
+		} else if requestData.Indexer == "ops" {
+			if requestData.OPSUserID == 0 {
+				log.Error().Msg("ops_user_id is missing but required when minratio is set for 'ops'")
+				http.Error(w, "ops_user_id is required for 'ops' when minratio is set", http.StatusBadRequest)
+				return
+			}
+			userID = requestData.OPSUserID
+			apiKey = requestData.OPSKey
+			//log.Trace().Msgf("MinRatio check for OPS with user ID: %d", userID)
+		}
+
+		if userID != 0 {
+			if userData == nil {
+				userData, err = fetchUserData(userID, apiKey, requestData.Indexer, apiBase)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+			}
+
+			ratio := userData.Response.Stats.Ratio
+			minRatio := requestData.MinRatio
+			username := userData.Response.Username
+
+			log.Trace().Msgf("MinRatio set to %.2f for %s on %s", minRatio, username, requestData.Indexer)
+
+			if ratio < minRatio {
+				w.WriteHeader(StatusRatioNotAllowed)
+				log.Debug().Msgf("Returned ratio %.2f is below minratio %.2f for %s on %s", ratio, minRatio, username, requestData.Indexer)
+				return
+			}
+		}
+	}
+
+	w.WriteHeader(http.StatusOK) // HTTP status code 200
+	log.Info().Msg("Conditions met, responding with status 200")
+}
--- config.go
+++ config.go
@@ -1,26 +1,36 @@
 package main
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/inhies/go-bytesize"
+	"github.com/natefinch/lumberjack"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
+const (
+	defaultConfigFileName = "config.toml"
+	defaultConfigType     = "toml"
+	defaultConfigDir      = ".config/redactedhook"
+	defaultLogLevel       = "trace"
+)
+
+var config Config
+
 type Config struct {
-	APIKeys     APIKeys `mapstructure:"apikeys"`
-	UserID      UserIDs `mapstructure:"userid"`
-	Ratio       Ratio   `mapstructure:"ratio"`
-	MinSize     int64   `mapstructure:"minsize"`
-	MaxSize     int64   `mapstructure:"maxsize"`
-	Uploaders   string  `mapstructure:"uploaders"`
-	RecordLabel string  `mapstructure:"record_labels"`
-	Mode        string  `mapstructure:"mode"`
-	Logs        Logs    `mapstructure:"logs"`
+	APIKeys     APIKeys   `mapstructure:"apikeys"`
+	UserID      UserIDs   `mapstructure:"userid"`
+	Ratio       Ratio     `mapstructure:"ratio"`
+	SizeCheck   SizeCheck `mapstructure:"sizecheck"`
+	ParsedSizes ParsedSizeCheck
+	Uploaders   Uploaders `mapstructure:"uploaders"`
+	Logs        Logs      `mapstructure:"logs"`
 }
 
 type APIKeys struct {
@@ -37,90 +47,192 @@ type Ratio struct {
 	MinRatio float64 `mapstructure:"minratio"`
 }
 
-type Logs struct {
-	LogLevel string `mapstructure:"loglevel"`
+type SizeCheck struct {
+	MinSize string `mapstructure:"minsize"`
+	MaxSize string `mapstructure:"maxsize"`
 }
 
-var config Config
+type ParsedSizeCheck struct {
+	MinSize bytesize.ByteSize
+	MaxSize bytesize.ByteSize
+}
+
+type Uploaders struct {
+	Uploaders string `mapstructure:"uploaders"`
+	Mode      string `mapstructure:"mode"`
+}
+
+type Logs struct {
+	LogLevel    string `mapstructure:"loglevel"`
+	LogToFile   bool   `mapstructure:"logtofile"`
+	LogFilePath string `mapstructure:"logfilepath"`
+	MaxSize     int    `mapstructure:"maxsize"`    // Max file size in MB
+	MaxBackups  int    `mapstructure:"maxbackups"` // Max number of old log files to keep
+	MaxAge      int    `mapstructure:"maxage"`     // Max age in days to keep a log file
+	Compress    bool   `mapstructure:"compress"`   // Whether to compress old log files
+}
 
 func initConfig(configPath string) {
-	var configFile string
+	configFile := determineConfigFile(configPath)
+	setupViper(configFile)
+	readAndUnmarshalConfig()
+	watchConfigChanges()
+}
 
-	if configPath == "" {
-		home, err := os.UserHomeDir()
-		if err != nil {
-			log.Error().Err(err).Msg("Failed to get user home directory, using current directory instead")
-			configFile = filepath.Join(".", "config.toml")
-		} else {
-			configDir := filepath.Join(home, ".config", "redactedhook")
-			configFile = filepath.Join(configDir, "config.toml")
-
-			// Try creating the directory
-			if _, err := os.Stat(configDir); os.IsNotExist(err) {
-				if mkdirErr := os.MkdirAll(configDir, os.ModePerm); mkdirErr != nil {
-					log.Error().Err(mkdirErr).Msg("Failed to create config directory in home, using current directory instead")
-					configFile = filepath.Join(".", "config.toml")
-				}
-			}
-		}
-	} else {
-		configFile = configPath
+func determineConfigFile(configPath string) string {
+	if configPath != "" {
+		return configPath
 	}
 
-	viper.SetConfigType("toml")
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get user home directory, using current directory instead")
+		return filepath.Join(".", defaultConfigFileName)
+	}
+
+	configDir := filepath.Join(home, defaultConfigDir)
+	configFile := filepath.Join(configDir, defaultConfigFileName)
+	if err := createConfigDirIfNotExist(configDir); err != nil {
+		log.Error().Err(err).Msg("Failed to create config directory, using current directory instead")
+		return filepath.Join(".", defaultConfigFileName)
+	}
+
+	return configFile
+}
+
+func createConfigDirIfNotExist(dir string) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return os.MkdirAll(dir, os.ModePerm)
+	}
+	return nil
+}
+
+func setupViper(configFile string) {
+	viper.SetConfigType(defaultConfigType)
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.SetConfigFile(configFile)
 
-	// Check if the config file exists, and if not, create it with default values
-	if _, err := os.Stat(configFile); os.IsNotExist(err) {
-		content := []byte(`[apikeys]
-#red_apikey = ""  # generate in user settings, needs torrent and user privileges
-#ops_apikey = ""  # generate in user settings, needs torrent and user privileges
-		
-[userid]
-#red_user_id = 0 # from /user.php?id=xxx
-#ops_user_id = 0 # from /user.php?id=xxx
-		
-[ratio]
-#minratio = 0.6 # reject releases if you are below this ratio
-
-[logs]
-loglevel = "trace" # trace, debug, info`)
-		err := os.WriteFile(configFile, content, 0644)
-		if err != nil {
-			log.Fatal().Err(err).Msg("Failed to create default config file")
-		}
-		log.Info().Msg("Created default config file")
-	} else if err != nil {
-		log.Fatal().Err(err).Msg("Failed to check if config file exists")
+	if err := createConfigFileIfNotExist(configFile); err != nil {
+		log.Fatal().Err(err).Msg("Failed to create or verify config file")
 	}
 
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatal().Err(err).Msg("Error reading config file")
 	}
+}
 
+func createConfigFileIfNotExist(configFile string) error {
+	if _, err := os.Stat(configFile); os.IsNotExist(err) {
+		defaultConfig := getDefaultConfig()
+		if err := os.WriteFile(configFile, defaultConfig, 0644); err != nil {
+			return err
+		}
+		log.Info().Msg("Created default config file")
+	}
+	return nil
+}
+
+func getDefaultConfig() []byte {
+	return []byte(`[apikeys]
+#red_apikey = ""  # generate in user settings, needs torrent and user privileges
+#ops_apikey = ""  # generate in user settings, needs torrent and user privileges
+
+[userid]
+#red_user_id = 0  # from /user.php?id=xxx
+#ops_user_id = 0  # from /user.php?id=xxx
+
+[ratio]
+#minratio = 0.6   # reject releases if you are below this ratio
+
+[sizecheck]
+#minsize = ""     # minimum size for checking, e.g., "10MB"
+#maxsize = ""     # maximum size for checking, e.g., "1GB"
+
+[uploaders]
+#uploaders = ""   # comma separated list of uploaders to allow
+#mode = ""        # whitelist or blacklist
+
+[logs]
+loglevel = "trace" # trace, debug, info
+logtofile = false  # Set to true to enable logging to a file
+logfilepath = ""   # Path to the log file
+maxsize = 0        # Max file size in MB
+maxbackups = 0     # Max number of old log files to keep
+maxage = 0         # Max age in days to keep a log file
+compress = false   # Whether to compress old log files
+`)
+}
+
+func readAndUnmarshalConfig() {
 	if err := viper.Unmarshal(&config); err != nil {
 		log.Error().Err(err).Msg("Unable to unmarshal config")
 	} else {
+		parseSizeCheck()
 		log.Debug().Msgf("Config file read successfully: %s", viper.ConfigFileUsed())
-		if config.Logs.LogLevel != "" {
-			setLogLevel(config.Logs.LogLevel)
-		} else {
-			log.Warn().Msg("Log level not specified in config, using default")
-			setLogLevel("debug")
+		configureLogger()
+	}
+}
+func parseSizeCheck() {
+	if config.SizeCheck.MinSize != "" && !strings.HasPrefix(config.SizeCheck.MinSize, "#") {
+		minSize, err := bytesize.Parse(config.SizeCheck.MinSize)
+		if err != nil {
+			log.Error().Err(err).Msg("Invalid format for minsize")
+			return
 		}
+		config.ParsedSizes.MinSize = minSize
 	}
 
+	if config.SizeCheck.MaxSize != "" && !strings.HasPrefix(config.SizeCheck.MaxSize, "#") {
+		maxSize, err := bytesize.Parse(config.SizeCheck.MaxSize)
+		if err != nil {
+			log.Error().Err(err).Msg("Invalid format for maxsize")
+			return
+		}
+		config.ParsedSizes.MaxSize = maxSize
+	}
+}
+
+func watchConfigChanges() {
 	viper.WatchConfig()
 	viper.OnConfigChange(func(e fsnotify.Event) {
 		log.Debug().Msgf("Config file updated: %s", e.Name)
+		oldLogLevel := config.Logs.LogLevel
 		if err := viper.Unmarshal(&config); err != nil {
 			log.Error().Err(err).Msg("Error reading config")
 		} else {
-			setLogLevel(config.Logs.LogLevel)
+			if oldLogLevel != config.Logs.LogLevel {
+				configureLogger()
+			}
 		}
 	})
+}
+
+func configureLogger() {
+	var writers []io.Writer
+
+	// Always log to console
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "2006-01-02 15:04:05"}
+	writers = append(writers, consoleWriter)
+
+	// If logtofile is true, also log to file
+	if config.Logs.LogToFile {
+		fileWriter := &lumberjack.Logger{
+			Filename:   config.Logs.LogFilePath,
+			MaxSize:    config.Logs.MaxSize,    // megabytes
+			MaxBackups: config.Logs.MaxBackups, // number of backups
+			MaxAge:     config.Logs.MaxAge,     // days
+			Compress:   config.Logs.Compress,   // compress rolling files
+		}
+		writers = append(writers, fileWriter)
+	}
+
+	// Combine all writers
+	multiWriter := zerolog.MultiLevelWriter(writers...)
+	log.Logger = zerolog.New(multiWriter).With().Timestamp().Logger()
+
+	// Set the log level
+	setLogLevel(config.Logs.LogLevel)
 }
 
 func setLogLevel(level string) {
@@ -137,5 +249,5 @@ func setLogLevel(level string) {
 		level = "debug"
 	}
 	zerolog.SetGlobalLevel(loglevel)
-	log.Debug().Msgf("Log level: %s", level)
+	log.Info().Msgf("Log level: %s", level)
 }
