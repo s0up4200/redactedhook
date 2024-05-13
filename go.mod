5bb5956d6f5518c1f71247d74db83f4bb05e49b6
s0up4200 <s0up4200@pm.me>
2024-05-13 22:28:02 +0200

    fix: tests

--- go.mod
+++ go.mod
@@ -8,16 +8,19 @@ require (
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/rs/zerolog v1.29.0
 	github.com/spf13/viper v1.17.0
+	github.com/stretchr/testify v1.8.4
 	golang.org/x/time v0.3.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
---
+++ internal/config/config_test.go
@@ -0,0 +1,83 @@
+package config
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupTestEnv() {
+	viper.Reset()
+	os.Clearenv()
+	viper.SetConfigType("toml")
+	viper.SetConfigFile("testconfig.toml")
+	viper.ReadConfig(bytes.NewReader([]byte(`
+	[authorization]
+	api_token = "test_token"
+	
+	[indexer_keys]
+	red_apikey = "red_key"
+	ops_apikey = "ops_key"
+	
+	[userid]
+	red_user_id = 1
+	ops_user_id = 2
+	
+	[ratio]
+	minratio = 0.5
+	
+	[sizecheck]
+	minsize = "10MB"
+	maxsize = "1GB"
+	
+	[uploaders]
+	uploaders = "test_uploader"
+	mode = "whitelist"
+	
+	[record_labels]
+	record_labels = "test_label"
+	
+	[logs]
+	loglevel = "debug"
+	logtofile = false
+	logfilepath = "test.log"
+	maxsize = 5
+	maxbackups = 2
+	maxage = 7
+	compress = false
+	
+	[server]
+	host = "127.0.0.1"
+	port = 42135
+	`)))
+}
+
+func TestValidateConfig(t *testing.T) {
+	setupTestEnv()
+	viper.Set("authorization.api_token", "")
+
+	err := ValidateConfig()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "Authorization API Token is required.")
+
+	viper.Set("authorization.api_token", "valid_token")
+	err = ValidateConfig()
+	assert.NoError(t, err)
+}
+
+func TestWatchConfigChanges(t *testing.T) {
+	setupTestEnv()
+
+	// simulate a config file change
+	viper.Set("server.port", 8080)
+	err := viper.WriteConfigAs("testconfig_updated.toml")
+	assert.NoError(t, err)
+
+	InitConfig("testconfig_updated.toml")
+	assert.Equal(t, 8080, config.Server.Port)
+
+	os.Remove("testconfig_updated.toml")
+}
