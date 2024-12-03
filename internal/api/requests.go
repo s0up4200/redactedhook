c02f58001d26431e68d95252668a50b013143a36
s0up4200 <s0up4200@pm.me>
2024-12-03 08:56:45 +0100

    fix(cache): include indexer in cache key to prevent collisions

--- internal/api/requests.go
+++ internal/api/requests.go
@@ -95,7 +95,7 @@ func initiateAPIRequest(id int, action, apiKey, apiBase, indexer string) (*Respo
 
 // fetchResponseData fetches response data from an API, checks the cache first, and caches the response data for future use.
 func fetchResponseData(requestData *RequestData, id int, action, apiBase string) (*ResponseData, error) {
-	cacheKey := fmt.Sprintf("%sID %d", action, id)
+	cacheKey := fmt.Sprintf("%s_%s_ID_%d", requestData.Indexer, action, id)
 	if cachedData, found := checkCache(cacheKey, requestData.Indexer); found {
 		return cachedData, nil
 	}
