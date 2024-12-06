ca394f0db85ca6f94d06c3fbec8bb658d2cd6799
s0up4200 <s0up4200@pm.me>
2024-12-06 08:08:05 +0100

    refactor(api): move request/response models to dedicated file

--- internal/api/hooks.go
+++ internal/api/hooks.go
@@ -9,48 +9,6 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-type RequestData struct {
-	REDUserID   int               `json:"red_user_id,omitempty"`
-	OPSUserID   int               `json:"ops_user_id,omitempty"`
-	TorrentID   int               `json:"torrent_id,omitempty"`
-	REDKey      string            `json:"red_apikey,omitempty"`
-	OPSKey      string            `json:"ops_apikey,omitempty"`
-	MinRatio    float64           `json:"minratio,omitempty"`
-	MinSize     bytesize.ByteSize `json:"minsize,omitempty"`
-	MaxSize     bytesize.ByteSize `json:"maxsize,omitempty"`
-	Uploaders   string            `json:"uploaders,omitempty"`
-	RecordLabel string            `json:"record_labels,omitempty"`
-	Mode        string            `json:"mode,omitempty"`
-	Indexer     string            `json:"indexer"`
-}
-
-type ResponseData struct {
-	Status   string `json:"status"`
-	Error    string `json:"error"`
-	Response struct {
-		Username string `json:"username"`
-		Stats    struct {
-			Ratio float64 `json:"ratio"`
-		} `json:"stats"`
-		Group struct {
-			Name      string `json:"name"`
-			MusicInfo struct {
-				Artists []struct {
-					ID   int    `json:"id"`
-					Name string `json:"name"`
-				} `json:"artists"`
-			} `json:"musicInfo"`
-		} `json:"group"`
-		Torrent *struct {
-			Username        string `json:"username"`
-			Size            int64  `json:"size"`
-			RecordLabel     string `json:"remasterRecordLabel"`
-			ReleaseName     string `json:"filePath"`
-			CatalogueNumber string `json:"remasterCatalogueNumber"`
-		} `json:"torrent"`
-	} `json:"response"`
-}
-
 func hookUploader(requestData *RequestData, apiBase string) error {
 	torrentData, err := fetchResponseData(requestData, requestData.TorrentID, "torrent", apiBase)
 	if err != nil {
